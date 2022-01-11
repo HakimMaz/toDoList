acb1324bee1de9df8f8244f2955560e4bb8e6b07
HakimMaz <maz.hakim93@gmail.com>
2022-01-11 09:57:11 +0100

    creating project

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  
+  return (
+    <SafeAreaView >
+      <Text>hello to do list </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
